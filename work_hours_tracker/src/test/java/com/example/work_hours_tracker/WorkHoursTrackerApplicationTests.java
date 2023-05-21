97d6eee0950359778a3797dd75d0c9ad1bfceecb
LukaStarcevic <luka.starcevic@rest-art.hr>
2023-05-21 15:19:39 +0200

    test

---
+++ work_hours_tracker/src/test/java/com/example/work_hours_tracker/WorkHoursTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.work_hours_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkHoursTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
